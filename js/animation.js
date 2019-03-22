45dac6dff1cce746ba2eadd643c5475bbe019dd8
Alexander Morozov <frostmoroz83@yandex.ru>
2019-03-22 11:36:44 +0400

    lesson6

---
+++ js/animation.js
@@ -0,0 +1,10 @@
+// let timerId = setTimeout(sayHello, 3000);
+// clearTimeout(timerId);
+
+
+// let timerId = setTimeout(function log() {
+//   console.log('Hello!');
+//   setTimeout(log, 2000);
+// });
+
+
